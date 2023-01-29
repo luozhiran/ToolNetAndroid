b682a959d2f1541fbd23f5947c08c7f24df863d5
罗志然 <luozhiran@100.me>
2023-01-29 11:51:09 +0800

    Initial commit

--- app/src/main/java/com/itg/ddlnet/MainActivity.kt
+++ app/src/main/java/com/itg/ddlnet/MainActivity.kt
@@ -16,7 +16,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         findViewById<Button>(R.id.send).setOnClickListener {
-            DdNet.instance.builder(DdNet.GET).send(object :DdCallback{
+            DdNet.instance.get().send(object :DdCallback{
                 override fun onFailure(er: String?) {
 
                 }
--- net/src/main/java/com/itg/net/DdNet.kt
+++ net/src/main/java/com/itg/net/DdNet.kt
@@ -3,6 +3,7 @@ package com.itg.net
 import com.itg.net.base.Builder
 import com.itg.net.download.CallbackMgr
 import com.itg.net.reqeust.create
+import com.itg.net.reqeust.model.Post
 import java.lang.Exception
 
 
@@ -11,34 +12,48 @@ class DdNet {
     companion object {
         @JvmStatic
         val GET = 1
+
         @JvmStatic
         val POST = 2
+
         @JvmStatic
         val DELETE = 3
+
         @JvmStatic
         val PUT = 4
+
         @JvmStatic
         val MEDIA_JSON = "application/json; charset=utf-8"
+
         @JvmStatic
         val MEDIA_OCTET_STREAM = "application/octet-stream"
+
         @JvmStatic
         val BROAD_ACTION = "com.yqtec.install.broadcast"
 
         @JvmStatic
-        val instance :DdNet by lazy { DdNet() }
+        val instance: DdNet by lazy { DdNet() }
     }
 
-    val ddNetConfig:DdNetConfig by lazy { DdNetConfig() }
-    val okhttpManager:OkhttpManager by lazy { OkhttpManager(ddNetConfig) }
-    val download:Download by lazy { Download() }
-    val callbackMgr:CallbackMgr by lazy { CallbackMgr() }
+    val ddNetConfig: DdNetConfig by lazy { DdNetConfig() }
+    val okhttpManager: OkhttpManager by lazy { OkhttpManager(ddNetConfig) }
+    val download: Download by lazy { Download() }
+    val callbackMgr: CallbackMgr by lazy { CallbackMgr() }
 
 
-    fun builder(type: Int): Builder {
+    private fun builder(type: Int): Builder {
         return create(type) ?: throw Exception("dot support $type")
     }
 
-    fun cancelAll(){
+    fun get() = builder(GET)
+
+    fun post() = builder(POST)
+
+    fun put() = builder(PUT)
+
+    fun delete() = builder(DELETE)
+
+    fun cancelAll() {
         okhttpManager.okHttpClient.dispatcher.queuedCalls().forEach {
             it.cancel()
         }
@@ -47,15 +62,15 @@ class DdNet {
         }
     }
 
-    fun cancelTag(tag:Any?){
-        if (tag == null)return
+    fun cancelTag(tag: Any?) {
+        if (tag == null) return
         okhttpManager.okHttpClient.dispatcher.queuedCalls().forEach {
-            if (tag == it.request().tag()){
+            if (tag == it.request().tag()) {
                 it.cancel()
             }
         }
         okhttpManager.okHttpClient.dispatcher.runningCalls().forEach {
-            if (tag == it.request().tag()){
+            if (tag == it.request().tag()) {
                 it.cancel()
             }
         }
